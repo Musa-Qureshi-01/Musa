613d5bf2962c60d8a692b6c04084cfb5f2684b29
Musa-Qureshi-01 <musaqureshi788code@gmail.com>
2026-07-08 22:35:57 +0530

    chore: integrate vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import "@fontsource/poppins";
 import "@fontsource/outfit";
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
